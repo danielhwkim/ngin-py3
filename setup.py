de4c867dd3b0cc951a965f89d70af5206464da5a
Daniel Kim <danielkim@daniels-mbp.lan>
2022-12-22 15:36:05 -0800

    first

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'ngin',
-    version = '0.3',
+    version = '0.4',
     license='MIT',
     description = 'ngin',
     author = 'Hoewon Kim',
